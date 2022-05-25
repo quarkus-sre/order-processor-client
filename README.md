c1a6b408335c7549cfbed3d89665b84ed3002891
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-05-25 14:17:40 -0300

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Should be deployed in the same project than order-processor service
--- client.sh
+++ client.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo '----------------------------'
-echo 'ORDER_URL: ' $ORDER_URL
-echo '----------------------------'
-for i in {0..10}
+#600 = 10 min
+#1800 = 30min
+for i in {0..1800}
   do 
-     curl -o /dev/null -s -w "%{http_code}\n" $ORDER_URL -H "Content-Type: application/json" -d @order.json
+     curl -o /dev/null -s -w "%{http_code}\n" "order-processor:8080/orders" -H "Content-Type: application/json" -d @order.json
      
      #curl -v $ORDER_URL -H "Content-Type: application/json" -d @order.json
      
